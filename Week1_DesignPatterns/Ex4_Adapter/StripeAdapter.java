9b7ac37a0953d0b9d561e03ce1148af952b5c9a2
angelferosha <angelferosha@gmail.com>
2026-06-16 10:45:59 +0530

    Delete Week1_DesignPatterns directory

--- Week1_DesignPatterns/Ex4_Adapter/StripeAdapter.java
+++
@@ -1,7 +0,0 @@
-public class StripeAdapter implements PaymentProcessor {
-    private StripeGateway stripeGateway = new StripeGateway();
-
-    public void processPayment(double amount) {
-        stripeGateway.sendPayment(amount);
-    }
-}
