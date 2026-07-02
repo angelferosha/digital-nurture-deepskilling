45be5e2cde1755c95ebdb0eb92ad3e381dd567e9
angelferosha <angelferosha@gmail.com>
2026-07-02 11:22:43 +0530

    Delete SpringCoreMaven directory

--- SpringCoreMaven/exercise9-spring-boot-app/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.repository;
-
-import com.library.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// Spring Data JPA generates the implementation at runtime --
-// findAll(), findById(), save(), deleteById(), etc. all come for free.
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
