afd0af4e88f504b35851384805423d8151b62e00
angelferosha <angelferosha@gmail.com>
2026-07-23 08:46:05 +0530

    Delete Week1_DesignPatterns directory

--- Week1_DesignPatterns/Ex4_Adapter/PayPalAdapter.java
+++
@@ -1,7 +0,0 @@
-public class PayPalAdapter implements PaymentProcessor {
-    private PayPalGateway payPalGateway = new PayPalGateway();
-
-    public void processPayment(double amount) {
-        payPalGateway.makePayment(amount);
-    }
-}
--- Week1_DesignPatterns/Ex4_Adapter/StripeGateway.java
+++
@@ -1,5 +0,0 @@
-public class StripeGateway {
-    public void sendPayment(double amount) {
-        System.out.println("Stripe: Sent payment of $" + amount);
-    }
-}
